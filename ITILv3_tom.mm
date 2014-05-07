1f488a26730b9dec95fdd963b85fed4ee869c97c
tstuckey <tstuckey@simplifyingcomplexspaces.com>
2014-05-07 13:43:41 -0700

    Initialization

---
+++ ITILv3_tom.mm
@@ -0,0 +1,333 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node FOLDED="false" ID="Freemind_Link_1" CREATED="1342674902826" MODIFIED="1398322115520"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      ITILv3 Processes
+    </p>
+    <p style="text-align: center">
+      Across the Service LIfecycle
+    </p>
+    <p style="text-align: center">
+      Phases
+    </p>
+  </body>
+</html>
+</richcontent>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffff00" BACKGROUND_COLOR="#0066cc" STYLE="bubble">
+<font SIZE="14"/>
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff" STYLE="bubble">
+<font SIZE="12"/>
+<edge WIDTH="4"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#330066" STYLE="fork">
+<font SIZE="10"/>
+<edge WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#000000" STYLE="fork">
+<font SIZE="10"/>
+<edge WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#000000" STYLE="fork">
+<font SIZE="10"/>
+<edge WIDTH="thin"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Service Strategy" POSITION="left" ID="Freemind_Link_10" CREATED="1342674902826" MODIFIED="1398321425063">
+<node TEXT="Demand Management" ID="Freemind_Link_11" CREATED="1342674902826" MODIFIED="1398321825252">
+<edge WIDTH="thin"/>
+<node TEXT="To analyze, track, monitor, and document Patterns of Business Activity (PBA)&#xa; to predict the current and future demand for services and enable the appropriate&#xa; capacity to be delivered at the right cost and at the right time." ID="ID_1008056856" CREATED="1398321372784" MODIFIED="1398321402493"/>
+</node>
+<node TEXT="Financial Management" ID="Freemind_Link_12" CREATED="1342674902827" MODIFIED="1398321825262">
+<edge WIDTH="thin"/>
+<node TEXT="To provide the business and IT with the quantification in financial terms&#xa;of the value of the assets underlying the provisioning of those services,&#xa;and the qualification of operational forecasting." ID="ID_1153434807" CREATED="1398321388713" MODIFIED="1398321411717"/>
+</node>
+<node TEXT="Service Portfoliio Management" ID="Freemind_Link_13" CREATED="1342674902827" MODIFIED="1398321825267">
+<edge WIDTH="thin"/>
+<node TEXT="To provide a dynamic method for governing investments in&#xa;service management across the enterprise, and managing them for value." ID="ID_705149163" CREATED="1398321623783" MODIFIED="1398321629876"/>
+</node>
+</node>
+<node TEXT="Service Design" POSITION="left" ID="Freemind_Link_14" CREATED="1342674902827" MODIFIED="1398321444648">
+<node TEXT="Supplier Management" ID="Freemind_Link_15" CREATED="1342674902828" MODIFIED="1398321825271">
+<edge WIDTH="thin"/>
+<node TEXT="To manage suppliers and the service the supply; to provide seamless quality&#xa; of IT services to the business, ensuring value for money is obtained." ID="ID_1576427281" CREATED="1398321638789" MODIFIED="1398321645724"/>
+</node>
+<node TEXT="Information Security Management" ID="Freemind_Link_16" CREATED="1342674902828" MODIFIED="1398321825276">
+<edge WIDTH="thin"/>
+<node TEXT="To align IT security with business security and ensure that information&#xa;security is effectively managed in all service and Service Management activities." ID="ID_969435698" CREATED="1398321657432" MODIFIED="1398321664940"/>
+</node>
+<node TEXT="Service Catalog Management" ID="Freemind_Link_17" CREATED="1342674902828" MODIFIED="1398321825280">
+<edge WIDTH="thin"/>
+<node TEXT="To manage the information in the Service Catalog and to ensure that it is&#xa; accurate and reflects the current details, status, interfaces, and dependencies&#xa;of all services that are being run or prepared to run in the live environment." ID="ID_744911433" CREATED="1398321681220" MODIFIED="1398321693388"/>
+</node>
+<node TEXT="Service Level Management" ID="Freemind_Link_18" CREATED="1342674902829" MODIFIED="1398321825285">
+<edge WIDTH="thin"/>
+<node TEXT="To ensure than an agreed-upon level of IT services is provided for all current&#xa;IT service and that future services are delivered to agreed-upon achievable&#xa;targets; proactive measures are also take to seek and implement improvements&#xa;to the level of service delivered." ID="ID_953086579" CREATED="1398321707666" MODIFIED="1398321725784"/>
+</node>
+<node TEXT="Capacity Management" ID="Freemind_Link_19" CREATED="1342674902829" MODIFIED="1398321825290">
+<edge WIDTH="thin"/>
+<node TEXT="To ensure that cost justifiable IT capacity in all areas of IT always exists and&#xa;is match to the current and future agreed-upon needs of the business in a timely manner." ID="ID_134853037" CREATED="1398321736044" MODIFIED="1398321743182"/>
+</node>
+<node TEXT="Availability Management" ID="Freemind_Link_20" CREATED="1342674902829" MODIFIED="1398321825293">
+<edge WIDTH="thin"/>
+<node TEXT="To ensure that the level of services availability delivered in all services&#xa; is matched to or exceeds the current and future agreed-upon needs of&#xa;the business in a cost-effective manner." ID="ID_1441493257" CREATED="1398321755544" MODIFIED="1398321777069"/>
+</node>
+<node TEXT="Continuity Management" ID="Freemind_Link_21" CREATED="1342674902830" MODIFIED="1398321825297">
+<edge WIDTH="thin"/>
+<node TEXT="To support the overall Business Continuity Management process by ensuring&#xa;that the required IT technical and service facilities can be resumed within&#xa;required and agreed-upon business timescales." ID="ID_770625036" CREATED="1398321787612" MODIFIED="1398321796236"/>
+</node>
+</node>
+<node TEXT="Anecdotal Summation:" POSITION="right" ID="ID_960110449" CREATED="1398322119002" MODIFIED="1398322261060">
+<node TEXT="Scenario: Constructing a new building&#xa;(courtesy of Mr. Juan Jim&#xe9;nez" ID="ID_1791183063" CREATED="1398322245465" MODIFIED="1398322547168">
+<node ID="ID_178179237" CREATED="1398322245465" MODIFIED="1398322307864"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Service Strategy: </b>
+    </p>
+    <p>
+      Meeting with potential tenants who might use the building
+    </p>
+    <p>
+      to see what they want/need; this results in a wants/needs list
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1782575713" CREATED="1398322245467" MODIFIED="1398322321596"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Service Design: </b>Taking the high-level requirements to an architect to
+    </p>
+    <p>
+      &#160;design a building that meets all or part of the wants/needs list; this
+    </p>
+    <p>
+      results in a set of blue-prints
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1948224086" CREATED="1398322245471" MODIFIED="1398322340417"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Service Transition:</b>&#160;
+    </p>
+    <p>
+      taking the blue prints to a general contactor
+    </p>
+    <p>
+      for implementation; this results in the desired building
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_393873319" CREATED="1398322245475" MODIFIED="1398322354461"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Service Operation</b>:
+    </p>
+    <p>
+      Providing the building superintendent a certificate-of-occupancy
+    </p>
+    <p>
+      &#160;so the tenants can use the building
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_482772917" CREATED="1398322245476" MODIFIED="1398322367173"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>CSI:</b>&#160;
+    </p>
+    <p>
+      Improving the building to meet the wants/needs of the tenants
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="CSI" POSITION="right" ID="Freemind_Link_2" CREATED="1342674902830" MODIFIED="1398321495449">
+<node TEXT="Define what one Should Measure" ID="Freemind_Link_3" CREATED="1342674902831" MODIFIED="1398321155196"/>
+<node TEXT="Define what one Can Measure" ID="Freemind_Link_4" CREATED="1342674902831" MODIFIED="1398321155199"/>
+<node TEXT="Get Data" ID="Freemind_Link_5" CREATED="1342674902831" MODIFIED="1398321155202"/>
+<node TEXT="Process the Data into Information" ID="Freemind_Link_6" CREATED="1342674902831" MODIFIED="1398321155205"/>
+<node TEXT="Analyze the Innformation and deduce Wisdom" ID="Freemind_Link_7" CREATED="1342674902831" MODIFIED="1398321155207"/>
+<node TEXT="Present findings" ID="Freemind_Link_8" CREATED="1342674902831" MODIFIED="1398321155209"/>
+<node TEXT="Take Action" ID="Freemind_Link_9" CREATED="1342674902832" MODIFIED="1398321600267">
+<node TEXT="Action Types:" ID="ID_1769115636" CREATED="1398321573857" MODIFIED="1398321573857">
+<node TEXT="Must Have" ID="ID_1454663234" CREATED="1398321573857" MODIFIED="1398321573857"/>
+<node TEXT="Should Have" ID="ID_1380920891" CREATED="1398321573857" MODIFIED="1398321573857"/>
+<node TEXT="Could Have" ID="ID_1137668576" CREATED="1398321573857" MODIFIED="1398321573857"/>
+<node TEXT="Would Like to Have" ID="ID_1071212381" CREATED="1398321573857" MODIFIED="1398321573857"/>
+</node>
+</node>
+</node>
+<node TEXT="Service Transition" POSITION="left" ID="Freemind_Link_22" CREATED="1342674902832" MODIFIED="1398321451880">
+<node TEXT="Service Transition Panning and Support" ID="Freemind_Link_23" CREATED="1342674902832" MODIFIED="1398321847872" COLOR="#276a5d" STYLE="fork">
+<font NAME="Serif" SIZE="14" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="thin"/>
+<node TEXT="Responsible for planning all Service Transition processes&#xa;and coordinating the resources they require." ID="ID_1576833831" CREATED="1398321849031" MODIFIED="1398321860984"/>
+</node>
+<node TEXT="Evaluation" ID="Freemind_Link_24" CREATED="1342674902833" MODIFIED="1398321869232" COLOR="#276a5d" STYLE="fork">
+<font NAME="Serif" SIZE="14" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="thin"/>
+<node TEXT="To assess a new or changes service to ensure that the risks have been&#xa; managed, and to help determine whether to proceed with the change." ID="ID_897920945" CREATED="1398321870826" MODIFIED="1398321877416"/>
+</node>
+<node TEXT="Change Management" ID="Freemind_Link_25" CREATED="1342674902833" MODIFIED="1398321891431" COLOR="#276a5d" STYLE="fork">
+<font NAME="Serif" SIZE="14" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="thin"/>
+<node TEXT="To respond to customers&apos; changing business requirements while maximizing value&#xa;and reducing incidents, disruption, and rework, and to respond to the business&#xa;and IT requests for change that will align the services with business needs." ID="ID_1298326794" CREATED="1398321892509" MODIFIED="1398321909096"/>
+</node>
+<node TEXT="Knowledge Management" ID="Freemind_Link_26" CREATED="1342674902833" MODIFIED="1398321917579" COLOR="#276a5d" STYLE="fork">
+<font NAME="Serif" SIZE="14" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="thin"/>
+<node TEXT="The process responsible for gathering, analyzing, storing,&#xa;and sharing knowledge and information within an organization." ID="ID_1084046966" CREATED="1398321919256" MODIFIED="1398321928297"/>
+</node>
+<node TEXT="Release and Deployment Management" ID="Freemind_Link_27" CREATED="1342674902834" MODIFIED="1398321938826" COLOR="#276a5d" STYLE="fork">
+<font NAME="Serif" SIZE="14" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="thin"/>
+<node TEXT="To build, test, and deliver the capability to provide the services specified&#xa; by the services design that will accomplish the stakeholders&apos; requirements&#xa;and deliver the intended objectives." ID="ID_1753201522" CREATED="1398321940435" MODIFIED="1398321949652"/>
+</node>
+<node TEXT="Service Validation and Testing" ID="Freemind_Link_28" CREATED="1342674902834" MODIFIED="1398321959012" COLOR="#276a5d" STYLE="fork">
+<font NAME="Serif" SIZE="14" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="thin"/>
+<node TEXT="The process responsible for validation and testing of a new or changed service;&#xa;it ensures that the service matches its design specification and will meet the needs of the business." ID="ID_1857982077" CREATED="1398321960773" MODIFIED="1398321967220"/>
+</node>
+<node TEXT="Service Asset and Confiugation Management" ID="Freemind_Link_29" CREATED="1342674902834" MODIFIED="1398321977563" COLOR="#276a5d" STYLE="fork">
+<font NAME="Serif" SIZE="14" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="thin"/>
+<node TEXT="To define and control the components of services and infrastructure,&#xa;and maintain accurate configuration records." ID="ID_548889298" CREATED="1398321978975" MODIFIED="1398321987432"/>
+</node>
+</node>
+<node TEXT="Service Operation" POSITION="left" ID="Freemind_Link_30" CREATED="1342674902835" MODIFIED="1398321467590">
+<node TEXT="Processes" ID="Freemind_Link_31" CREATED="1342674902835" MODIFIED="1398321811100" COLOR="#276a5d" STYLE="fork">
+<font NAME="Serif" SIZE="14" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="thin"/>
+<node TEXT="Incident Management" ID="Freemind_Link_32" CREATED="1342674902835" MODIFIED="1398321998086" COLOR="#330033" STYLE="fork">
+<font NAME="SansSerif" SIZE="12" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="2"/>
+<node TEXT="To restore normal service operation as quickly as possible, thus ensuring&#xa;that the best possible levels of service quality and availability are maintained." ID="ID_1331527617" CREATED="1398321999314" MODIFIED="1398322009495"/>
+</node>
+<node TEXT="Request Fulfillment" ID="Freemind_Link_33" CREATED="1342674902836" MODIFIED="1398322020388" COLOR="#330033" STYLE="fork">
+<font NAME="SansSerif" SIZE="12" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="2"/>
+<node TEXT="To provide a channel for users to request and receive standard pre-defined&#xa;and pre-approved services and information relating to the availability of standard services." ID="ID_1727300680" CREATED="1398322021493" MODIFIED="1398322030988"/>
+</node>
+<node TEXT="Event Mangement" ID="Freemind_Link_34" CREATED="1342674902836" MODIFIED="1398322044661" COLOR="#330033" STYLE="fork">
+<font NAME="SansSerif" SIZE="12" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="2"/>
+<node TEXT="To provide the ability to detect event, evaluate them, and then determine the&#xa;appropriate control action and to act as the basis and entry point for the automation&#xa;and execution of routine operational tasks." ID="ID_1080236572" CREATED="1398322045656" MODIFIED="1398322054659"/>
+</node>
+<node TEXT="Access Management" ID="Freemind_Link_35" CREATED="1342674902836" MODIFIED="1398322064096" COLOR="#330033" STYLE="fork">
+<font NAME="SansSerif" SIZE="12" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="2"/>
+<node TEXT="To grant authorized users the right to use a service while preventing access&#xa;to unauthorized users and execute policies and actions defined in Information Security and Availability Management." ID="ID_817125898" CREATED="1398322065371" MODIFIED="1398322072960"/>
+</node>
+<node TEXT="Problem Management" ID="Freemind_Link_36" CREATED="1342674902837" MODIFIED="1398322081947" COLOR="#330033" STYLE="fork">
+<font NAME="SansSerif" SIZE="12" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="2"/>
+<node TEXT="To prevent problems and resulting incidents from happening; to eliminate recurring incidents;&#xa;and to minimize the impact of incidents that cannot be prevented." ID="ID_1283254455" CREATED="1398322085893" MODIFIED="1398322091573"/>
+</node>
+</node>
+<node TEXT="Functions" ID="Freemind_Link_37" CREATED="1342674902837" MODIFIED="1398321811103" COLOR="#276a5d" STYLE="fork">
+<font NAME="Serif" SIZE="14" BOLD="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="thin"/>
+<node TEXT="IT Operations and Facilities" ID="Freemind_Link_38" CREATED="1342674902837" MODIFIED="1342674927876" COLOR="#330033" STYLE="fork">
+<font NAME="SansSerif" SIZE="12" ITALIC="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="2"/>
+</node>
+<node TEXT="Application Management" ID="Freemind_Link_39" CREATED="1342674902837" MODIFIED="1342674927877" COLOR="#330033" STYLE="fork">
+<font NAME="SansSerif" SIZE="12" ITALIC="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="2"/>
+</node>
+<node TEXT="Technical Management" ID="Freemind_Link_40" CREATED="1342674902838" MODIFIED="1342674927877" COLOR="#330033" STYLE="fork">
+<font NAME="SansSerif" SIZE="12" ITALIC="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="2"/>
+</node>
+<node TEXT="Service Desk" ID="Freemind_Link_41" CREATED="1342674902838" MODIFIED="1342674927877" COLOR="#330033" STYLE="fork">
+<font NAME="SansSerif" SIZE="12" ITALIC="true"/>
+<edge STYLE="sharp_bezier" COLOR="#999999" WIDTH="2"/>
+</node>
+</node>
+</node>
+</node>
+</map>
